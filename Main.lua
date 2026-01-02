26e1db402c75ab28fda09e415b7772c13f9db76b
mm2dino123-sketch <mm2dino123@gmail.com>
2026-01-01 19:30:11 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,14 @@
+--// Dino's Remote Script
+print("✅ Chilli-style loader activated!")
+
+-- Example functionality
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+print("Hello, " .. player.Name .. "! Your UserId is " .. player.UserId)
+
+-- You can add more features here:
+-- - GUI toggle
+-- - Anti-tamper
+-- - Key system
+-- - Multi-script loader
