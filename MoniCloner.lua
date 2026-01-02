f773bad3828a8cc58f9312c25c041d7352abddad
mm2dino123-sketch <mm2dino123@gmail.com>
2026-01-02 00:51:25 -0500

    Update MoniCloner.lua

--- MoniCloner.lua
+++ MoniCloner.lua
@@ -1,2 +1,12 @@
-game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Net"):WaitForChild("RE/UseItem"):FireServer()
-game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Net"):WaitForChild("RE/QuantumCloner/OnTeleport"):FireServer()
+-- Listen for key press
+local UserInputService = game:GetService("UserInputService")
+
+UserInputService.InputBegan:Connect(function(input, gameProcessed)
+    if gameProcessed then return end
+    if input.KeyCode == Enum.KeyCode.V then
+        print("V pressed — loading MoniCloner")
+
+        -- Run your loadstring
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/mm2dino123-sketch/moni/main/MoniCloner.lua"))()
+    end
+end)
